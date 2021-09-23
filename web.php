261322e0b8fc73bf78d3410551fbe8a381292544
Chiba Takaki <takaki200109@gmail.com>
2021-09-23 13:08:03 +0900

    k10

---
+++ web.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::group(['prefix' => 'XXX'], funciton() {
+  Route::get('XXX', 'XXX\AAAController@bbb');
+});
+
+Route::group(['prefix' => 'admin'], funciton() {
+  Route::get('news/create', 'Admin\NewsController@add');
+  Route::get('profile/create', 'Admin\ProfileController@add');
+  Route::get('profile/edit', 'Admin\ProfileController@edit');
+});
